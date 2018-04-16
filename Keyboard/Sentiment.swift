46d430e8867f9a78f5671eb4dffd50321bbf8c85
Cameron Deardorff <camdeardorff@camdeardorff.com>
2018-04-16 16:11:15 -0400

    formatting

--- Keyboard/Sentiment.swift
+++ Keyboard/Sentiment.swift
@@ -1,29 +1,40 @@
 import UIKit
 
 enum Sentiment {
-  case neutral
-  case positive
-  case negative
-
-  var emoji: String {
-    switch self {
-    case .neutral:
-      return "😐"
-    case .positive:
-      return "😃"
-    case .negative:
-      return "😔"
+    case neutral
+    case positive
+    case negative
+    
+    var emoji: String {
+        switch self {
+        case .neutral:
+            return "😐"
+        case .positive:
+            return "😃"
+        case .negative:
+            return "😔"
+        }
     }
-  }
-
-  var color: UIColor? {
-    switch self {
-    case .neutral:
-      return UIColor.SA_Blue
-    case .positive:
-      return UIColor.SA_Green
-    case .negative:
-      return UIColor.SA_Red
+    
+    var color: UIColor? {
+        switch self {
+        case .neutral:
+            return UIColor.SA_Blue
+        case .positive:
+            return UIColor.SA_Green
+        case .negative:
+            return UIColor.SA_Red
+        }
     }
-  }
+    
+    static func from(classLabel: String) -> Sentiment {
+        switch classLabel {
+        case "positive":
+            return .positive
+        case "negative":
+            return .negative
+        default:
+            return .neutral
+        }
+    }
 }
