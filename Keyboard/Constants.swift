b8e36defd454cabe3e05916a01d400e893c08354
Cameron Deardorff <camdeardorff@camdeardorff.com>
2018-05-03 00:45:12 -0400

    added extensions for simplifying the decoding process

--- Keyboard/Constants.swift
+++ Keyboard/Constants.swift
@@ -44,3 +44,37 @@ extension Array {
         return self[index]
     }
 }
+
+public struct Safe<Base: Decodable>: Decodable {
+    public let value: Base?
+    
+    public init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.singleValueContainer()
+            self.value = try container.decode(Base.self)
+        } catch {
+            assertionFailure("ERROR: \(error)")
+            // TODO: automatically send a report about a corrupted data
+            self.value = nil
+        }
+    }
+}
+extension KeyedDecodingContainer {
+    public func decodeSafely<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
+        return self.decodeSafely(T.self, forKey: key)
+    }
+    
+    public func decodeSafely<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
+        let decoded = try? decode(Safe<T>.self, forKey: key)
+        return decoded?.value
+    }
+    
+    public func decodeSafelyIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
+        return self.decodeSafelyIfPresent(T.self, forKey: key)
+    }
+    
+    public func decodeSafelyIfPresent<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
+        let decoded = try? decodeIfPresent(Safe<T>.self, forKey: key)
+        return decoded??.value
+    }
+}
